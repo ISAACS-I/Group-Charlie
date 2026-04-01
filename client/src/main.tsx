5cf3f3d01f1c6af36eb55b30f2fdccc0c6acc284
Isago <Isago@dynamic-005-004-014-011.5.4.pool.telefonica.de>
2026-04-01 23:31:37 +0200

    Restore main.tsx content

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+import App from "./App";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
